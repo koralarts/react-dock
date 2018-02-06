0c583c0265973841b7b1ecf762a2b541e661314d
Karl Castillo <khcastillo@hotmail.com>
2018-02-06 15:40:16 -0800

    Added bottom and right to make it full width and height

--- src/Dock.js
+++ src/Dock.js
@@ -13,10 +13,10 @@ function autoprefixes(styles) {
 const styles = autoprefixes({
   wrapper: {
     position: 'fixed',
-    width: 0,
-    height: 0,
     top: 0,
-    left: 0
+    left: 0,
+    right: 0,
+    bottom: 0
   },
 
   dim: {
